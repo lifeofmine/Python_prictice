b4c6de38a5f6d4c7812a3fc1a644b73a23c2e45b
lifeofmine <uccent@sina.com>
2018-05-03 17:54:08 +0800

    Day1 Web APP

---
+++ www/app.py
@@ -0,0 +1,30 @@
+# python
+# -*- coding:utf-8 -*-
+# time :2018/5/3 17:32
+# @Author: 50433
+# @File:.py
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+
+def index(request):
+    return web.Response(body='<h1>Awesome</h1>')
+
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
